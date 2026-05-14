0f166edbe6b1b19ee37cfb17f9b9d42efacf98ae
Nguyễn Thị Thu Hiền <nguyentth0909@gmail.com>
2026-05-14 08:18:14 +0700

    commit

---
+++ HCM_KS25_CSDL_SS12_CNTT7_NguyenThiThuHien.sql
@@ -0,0 +1,117 @@
+CREATE DATABASE StudentDB;
+USE StudentDB;
+
+-- 1. Bảng Khoa
+CREATE TABLE Department (
+    DeptID VARCHAR(5) PRIMARY KEY,
+    DeptName VARCHAR(50) NOT NULL
+);
+
+-- 2. Bảng SinhVien
+CREATE TABLE Student (
+    StudentID VARCHAR(6) PRIMARY KEY,
+    FullName VARCHAR(50),
+    Gender VARCHAR(10),
+    BirthDate DATE,
+    DeptID VARCHAR(5),
+    FOREIGN KEY (DeptID) REFERENCES Department(DeptID)
+);
+
+-- 3. Bảng MonHoc
+CREATE TABLE Course (
+    CourseID VARCHAR(6) PRIMARY KEY,
+    CourseName VARCHAR(50),
+    Credits INT
+);
+
+-- 4. Bảng DangKy
+CREATE TABLE Enrollment (
+    StudentID VARCHAR(6),
+    CourseID VARCHAR(6),
+    Score DECIMAL(4,2), 
+    PRIMARY KEY (StudentID, CourseID),
+    FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
+    FOREIGN KEY (CourseID) REFERENCES Course(CourseID)
+);
+
+-- Chèn dữ liệu mẫu
+INSERT INTO Department VALUES
+('IT','Information Technology'),
+('BA','Business Administration'),
+('ACC','Accounting');
+
+INSERT INTO Student VALUES
+('S00001','Nguyen An','Male','2003-05-10','IT'),
+('S00002','Tran Binh','Male','2003-06-15','IT'),
+('S00003','Le Hoa','Female','2003-08-20','BA'),
+('S00004','Pham Minh','Male','2002-12-12','ACC'),
+('S00005','Vo Lan','Female','2003-03-01','IT'),
+('S00006','Do Hung','Male','2002-11-11','BA'),
+('S00007','Nguyen Mai','Female','2003-07-07','ACC'),
+('S00008','Tran Phuc','Male','2003-09-09','IT');
+
+insert into Course value
+('C00001', 'Database System', 500000),
+('C00002', 'Python', 800000),
+('C00003', 'C/C++', 500000);
+
+insert into Enrollment value
+('S00001', 'C00001', 5.6),
+('S00008', 'C00001', 8.9),
+('S00004', 'C00001', 10),
+('S00006', 'C00001', 7.5);
+
+-- Câu 1: Tạo View ViewStudentBasic hiển thị: StudentID, FullName, và DeptName. Sau đó viết lệnh truy vấn toàn bộ dữ liệu từ View này.
+create view ViewStudentBasic as
+select StudentID, FullName, DeptName
+from Student s
+join Department d
+on s.DeptID = d.DeptID;
+
+select * from ViewStudentBasic;
+
+-- Câu 2: Tạo một Regular Index tên là idxFullName cho cột FullName của bảng Student.
+create index idxFullName
+on Student(FullName);
+
+-- Câu 3: Viết Stored Procedure GetStudentsIT (không có tham số).
+-- Chức năng: Hiển thị toàn bộ sinh viên thuộc khoa "Information Technology" trong bảng Student kết hợp với DeptName từ bảng Department.
+-- Yêu cầu: Gọi procedure bằng lệnh CALL để kiểm tra.
+delimiter //
+create procedure GetStudentsIT()
+begin
+	select StudentID, FullName, gender, DeptName
+    from Student s
+    join Department d
+    on s.DeptID = d.DeptID
+    where DeptName = 'Information Technology';
+end //
+delimiter ;
+
+call GetStudentsIT();
+
+-- Câu 4:
+-- a) Tạo View ViewStudentCountByDept hiển thị: DeptName, TotalStudents (số lượng sinh viên của mỗi khoa).
+create view ViewStudentCountByDept as
+select DeptName,  count(s.DeptID) TotalStudents
+from Department d
+join Student s
+on s.DeptID = d.DeptID
+group by DeptName;
+
+-- b) Từ View trên, viết truy vấn hiển thị khoa có nhiều sinh viên nhất
+select *
+from ViewStudentCountByDept
+order by TotalStudents desc
+limit 1;
+
+-- Câu 5:
+-- a) Viết Stored Procedure GetTopScoreStudent với tham số: IN varCourseID VARCHAR(6).
+-- Chức năng: Hiển thị sinh viên có điểm cao nhất trong môn học được truyền vào.
+delimiter //
+create procedure GetTopScoreStudent(in varCourseID varchar(6))
+begin
+	select 
+end //
+delimiter ;
+-- b) Gọi thủ tục trên để tìm sinh viên có điểm cao nhất môn "Database Systems" (C00001).
